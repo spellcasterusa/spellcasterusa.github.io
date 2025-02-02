050705feb652333cb32990e38f12eb2ec1484972
spellcasterusa <nakamatte558@gmail.com>
2025-02-02 21:50:12 +0200

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-31.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-46a05162', {"layout":"number","heading":"Money Spells","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":35,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-874b77cb', {"layout":"number","heading":"Happy Clients","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":140,"totalNumber":1400,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-75b73eb3', {"layout":"number","heading":" Member","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":20,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-fcff0d9a' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-63e6dab0' );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-32.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-fcff0d9a' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-63e6dab0' );
+});
+ });
